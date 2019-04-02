9ead3b08f39e7365a308723925df5d4bc7a7e140
Roger <suncowbee@163.com>
2019-04-02 16:52:33 +0800

    create a block

--- src/main.go
+++ src/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main(){
-	
+import "fmt"
+
+type Block struct {
+	PreHash []byte
+	Hash    []byte
+	Data    []byte
+}
+
+func NewBlock(data string, preHash []byte) *Block{
+	block:=Block{
+		PreHash: preHash,
+		Hash:[]byte{},
+		Data:[]byte(data),
+	}
+	return &block
+}
+
+func main() {
+	block := NewBlock("helloworld", []byte{})
+	fmt.Printf("%x\n",block.PreHash)
+	fmt.Printf("%x\n",block.Hash)
+	fmt.Printf("%x\n",block.Data)
 }
